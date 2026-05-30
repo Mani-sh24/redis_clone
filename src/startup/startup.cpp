3faea229ce0cab07d0e831ba3d322671b0a7e7dd
Mani-sh24 <manish.adhikari.2402@gmail.com>
2026-05-30 18:13:42 +0530

    Art

--- src/startup/startup.cpp
+++ src/startup/startup.cpp
@@ -3,7 +3,19 @@
 
 void startup_info(__uint16_t &port){
     pid_t pid = getpid();
-    std::cout << "===========================================================" << std::endl;
-    std::cout << "Pid = " << pid << std::endl << "Port " << port << std::endl;
-    std::cout << "===========================================================" << std::endl;
+    pid_t ppid = getppid();
+    uid_t uid = getuid();
+    
+    std::cout << "                _._" << std::endl;
+    std::cout << "           _.-``__ ''-._" << std::endl;
+    std::cout << "      _.-``    `.  `_.  ''-._          Redis C++ Clone" << std::endl;
+    std::cout << "  .-`` .-```.  ```\\/    _.,_ ''-._     Port: " << port << std::endl;
+    std::cout << " (    ' (＃｀д´)     .-`  | `,    )    PID:  " << pid << std::endl;
+    std::cout << " |`-._`-...-` __...-.``-._|'` _.-'|    PPID: " << ppid << std::endl;
+    std::cout << " |    `-._   `._    /     _.-'    |    UID:  " << uid << std::endl;
+    std::cout << "  `-._    `-._  `-./___.-'    _.-'" << std::endl;
+    std::cout << "      `-._    `-._   _.-'  _.-'" << std::endl;
+    std::cout << "          `-._    `-._ _.-'" << std::endl;
+    std::cout << "              `-._  _.-'" << std::endl;
+    std::cout << "                  `-'" << std::endl << std::endl;
 }
